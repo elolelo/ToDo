1ac5573a1f055724c80aad4cb7f239881c54e080
Nompumelelo <lelonompumelelo@gmail.com>
2019-04-06 07:52:41 +0200

    added logic to the addItem function

--- src/TodoList.js
+++ src/TodoList.js
@@ -12,6 +12,14 @@ class TodoList extends Component{
     }
 
     addItem(e){
+        if (this._inputElement != ""){
+            let newItem = {
+                text: this._inputElement.value,
+                key: Date.now(),
+            };
+
+
+        }
     }
     render(){
         return (
